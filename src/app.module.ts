237b78681f1ea5abf4bbb8442fde3120bfa9ba72
rbdus0715 <rbdus0715@gmail.com>
2025-07-31 23:36:55 +0900

    Init: add mysql typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '1234',
+      database: 'publit',
+      entities: [__dirname + '/apis/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      logging: true,
+    }),
+  ],
 })
 export class AppModule {}
