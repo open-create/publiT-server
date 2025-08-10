95d6f670b20e0b408d8434fc5d6e02686a01a649
rbdus0715 <rbdus0715@gmail.com>
2025-08-10 11:04:41 +0900

    Feat: naver jwt strategy 추가

--- src/apis/auth/strategies/jwt-social-naver.strategy.ts
+++ src/apis/auth/strategies/jwt-social-naver.strategy.ts
@@ -2,5 +2,33 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-google-oauth20';
 
 export class JwtGoogleStrategy extends PassportStrategy(Strategy, 'naver') {
-  validate() {}
+  constructor() {
+    const clientID = process.env.NAVER_SOCIAL_CLIENT_ID;
+    const clientSecret = process.env.NAVER_SOCIAL_CLIENT_SECRET;
+    const serverURL = process.env.SERVER_URL;
+
+    console.log(clientID, clientSecret, serverURL);
+    if (!clientID || !clientSecret || !serverURL) {
+      throw new Error('Missing Google OAuth environment variables');
+    }
+
+    super({
+      clientID,
+      clientSecret,
+      callbackURL: serverURL + '/auth/login-naver',
+      // scope: ['email', 'profile'],
+    });
+  }
+
+  validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: import('passport-google-oauth20').Profile,
+  ) {
+    return {
+      userName: profile.displayName,
+      email: profile.emails && profile.emails[0] ? profile.emails[0].value : '',
+      password: '1234',
+    };
+  }
 }
