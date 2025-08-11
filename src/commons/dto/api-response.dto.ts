6b17248d7b08b3192562219f6b63527333093466
rbdus0715 <rbdus0715@gmail.com>
2025-08-11 14:21:38 +0900

    Refactor: ApiResponse<T> 추가

---
+++ src/commons/dto/api-response.dto.ts
@@ -0,0 +1,21 @@
+export class ApiResponse<T> {
+  success: boolean;
+  data?: T;
+  message?: string;
+  code?: string;
+
+  constructor(success: boolean, message: string, data?: T, code?: string) {
+    this.success = success;
+    this.data = data;
+    this.message = message;
+    this.code = code;
+  }
+
+  static success<T>(message: string, data?: T): ApiResponse<T> {
+    return new ApiResponse(true, message, data);
+  }
+
+  static error(message: string, code?: string) {
+    return new ApiResponse(false, message, undefined, code);
+  }
+}
