fab08017cd3688ad876556d9c7c45395ea0363c0
rbdus0715 <rbdus0715@gmail.com>
2025-08-15 17:24:58 +0900

    Feat: 알람 엔티티

--- src/apis/notices/entities/notice.entity.ts
+++ src/apis/notices/entities/notice.entity.ts
@@ -1,4 +1,38 @@
-import { Entity } from 'typeorm';
+import { User } from 'src/apis/users/entities/user.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+enum NoticeType {
+  POST = 'POST',
+  COMMENT = 'COMMENT',
+  SUBSCRIPTION = 'SUBSCRIPTION',
+  PUBBLE_SAVE = 'PUBBLE_SAVE',
+  PUBBLE_COMMENT = 'PUBBLE_COMMENT',
+  OFFICIAL = 'OFFICIAL',
+}
 
 @Entity()
-export class Notice {}
+export class Notice {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column()
+  title: string;
+
+  @ManyToOne(() => User, (user) => user.notifications, { onDelete: 'CASCADE' })
+  receiver: User;
+
+  @Column({ type: 'enum', enum: NoticeType })
+  type: NoticeType;
+
+  @Column({ type: 'varchar', length: 255 })
+  message: string;
+
+  @Column()
+  referencedId: string;
+
+  @Column({ default: false })
+  isRead: boolean;
+
+  @Column()
+  createdAt: Date;
+}
