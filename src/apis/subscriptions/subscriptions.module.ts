11654bc0dddcc3a497f9f068e3519b00c065472b
rbdus0715 <rbdus0715@gmail.com>
2025-08-15 11:53:44 +0900

    Feat: 구독, 취소 기능 추가

---
+++ src/apis/subscriptions/subscriptions.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Subscription } from './entities/subscription.entity';
+import { SubscriptionsService } from './subscriptions.service';
+import { SubscriptionsController } from './subscriptions.controller';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      Subscription, //
+    ]),
+  ],
+  controllers: [SubscriptionsController],
+  providers: [SubscriptionsService],
+})
+export class SubscriptionsModule {}
