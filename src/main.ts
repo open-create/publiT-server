82847f79f7d150f80afe7b75fc2166448b957b84
rbdus0715 <rbdus0715@gmail.com>
2025-08-20 03:56:15 +0900

    Chore: 프론트 서버 주소 cors 허용

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import { TransformInterceptor } from './commons/interceptors/reponse.interceptor
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3000',
+      'https://publit-client-50435321099.asia-northeast3.run.app',
+    ],
     methods: 'GET, POST, PATCH, DELETE, OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
